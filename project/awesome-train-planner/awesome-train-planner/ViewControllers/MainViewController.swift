d42c32b348fee76b28dc3266206dcff8df2543f1
Vladimir Amiorkov <v.amiorkov@gmail.com>
2020-04-25 16:42:23 +0300

    chore: update project directory structure for mvvm

--- project/awesome-train-planner/awesome-train-planner/ViewControllers/MainViewController.swift
+++ project/awesome-train-planner/awesome-train-planner/ViewControllers/MainViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  MainViewController.swift
 //  awesome-train-planner
 //
 //  Created by Vladimir Amiorkov on 25.04.20.
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class MainViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
