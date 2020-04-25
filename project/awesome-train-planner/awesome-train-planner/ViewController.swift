b08d0ca31b00cf24757c8738f7fd5504e509a1ea
Vladimir Amiorkov <v.amiorkov@gmail.com>
2020-04-25 16:16:46 +0300

    chore: add initial blank xcode project

---
+++ project/awesome-train-planner/awesome-train-planner/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  awesome-train-planner
+//
+//  Created by Vladimir Amiorkov on 25.04.20.
+//  Copyright © 2020 Vladimir Amiorkov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
